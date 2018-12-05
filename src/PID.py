c0126a0b881c4f985f47fe746a66f188cbe82301
olindenaus <madorgon@gmail.com>
2018-12-05 12:30:01 +0100

    wykresy, zapis do pliku, sterowanie przeplywem

--- src/PID.py
+++ src/PID.py
@@ -3,15 +3,17 @@ class PID:
     ki = 0
     kd = 0
     last_error = 0.0
-    last_sum = 0.0
+    integral = 0.0
+    derivative = 0.0
 
     def __init__(self, P, I, D):
         self.kp = P
         self.ki = I
         self.kd = D
 
-    def regulate(self, error, tn):
-        self.last_sum = error + self.last_sum
-        output = (self.kp + self.ki * self.last_sum + self.kd * (error - self.last_error)) * tn
-        self.last_error = error
+    def regulate(self, system_error, tn):
+        self.integral = system_error * tn + self.integral
+        self.derivative = (system_error - self.last_error) / tn
+        output = self.kp*system_error + self.ki * self.integral + self.kd * self.derivative
+        self.last_error = system_error
         return output
--- src/main.py
+++ src/main.py
@@ -1,54 +1,86 @@
-import time as tm
-from math import *
-import math
 from PID import PID
-
 import matplotlib.pyplot as plt
 
 
-def model():
-    czas_probkowania = 0.01
-    last_V = 50.0
-    last_CA = 0
-    CA = 0.0
-    CA_zadane = 0.4
-    czas_symulacji = 0
-    q1 = 1
-    q2 = 2
-    q3 = 3
+def main():
+    sample_time = 0.01  # czas próbkowania
+    last_v = 50.0  # objętość w poprzedniej próbce czasu
+    last_ca = 0  # stężenie w poprzedniej próbce czasu
+    current_ca = 0.0  # stężenie w danej próbce czasu
+    given_ca = 0.4  # zadane stężenie
+    given_substance_volume = given_ca * last_v  # zadana objętość substancji w zbiorniku
+    simulation_time = 0  # czas symulacji
+    q2 = 2  # prędkość przepływu strumienia zakłócającego
+    q3 = 3  # prędkość opdływu cieczy ze zbiornika
+    c1 = 0.7  # stężenie w strumieniu sterowanym
+    c2 = 0.3  # stężenie w strumieniu zakłócającym
 
-    c2 = 0.3
+    ca_samples = []  # próbki stężenia w zbiorniku
+    q1_samples = []  # próbki prędkości przepływu
+    substance_volume_samples = []  # próbki objętości substancji
+    t_samples = []  # próbki czasu
+    v_samples = []  # próbki objętości
+    controller = PID(1, 0, 0.1)
 
-    CA_samples = []
-    C1_samples = []
-    t_samples = []
-    controller = PID(12, 0.1, 0.1)
+    while simulation_time < 200:
+        simulation_time += sample_time
+        current_substance_volume = current_ca * last_v
+        system_error = given_substance_volume - current_substance_volume
+        q1 = controller.regulate(system_error, sample_time) / c1
+        q1 = min(q1, 5.0)
+        q1 = max(q1, 0.0)
 
-    while czas_symulacji < 600:
-        czas_symulacji += czas_probkowania
+        current_v = (q1 + q2 - q3) * sample_time + last_v
+        current_ca = ((c1 * q1 + c2 * q2) * sample_time + current_v * last_ca) / (
+                2 * current_v - last_v + q3 * sample_time)
 
-        E = CA_zadane - CA
-        c1 = controller.regulate(E, czas_probkowania)
-        c1 = min(c1, 1.0)
-        C1_samples.append(c1)
+        ca_samples.append(current_ca*100)
+        v_samples.append(current_v)
+        t_samples.append(simulation_time)
+        q1_samples.append(q1)
+        substance_volume_samples.append(current_substance_volume)
 
-        V = (q1 + q2 - q3) * czas_probkowania + last_V
-        CA = ((c1 * q1 + c2 * q2) * czas_probkowania + V * last_CA) / (2 * V - last_V + q3 * czas_probkowania)
-        CA_samples.append(CA)
+        last_ca = current_ca
+        last_v = current_v
 
-        t_samples.append(czas_symulacji)
+    save_to_file('prędkość_przepływu_próbki', q1_samples)
+    save_to_file('objętość_w_zbiorniku_próbki', v_samples)
+    save_to_file('substancja_w_zbiorniku_próbki', substance_volume_samples)
+    save_to_file('próbki_czasu', t_samples)
 
-        last_CA = CA
-        last_V = V
 
+    plt.figure('Wykres objętości')
+    plt.ylabel('objętość [m3]')
+    plt.xlabel('czas [s]')
+    plt.plot(t_samples, substance_volume_samples, label='Objętość substancji w zbiorniku')
+    plt.title('V(t)')
 
-    plt.plot(t_samples, C1_samples)
-    plt.plot(t_samples, CA_samples)
+    plt.xlabel('czas [s]')
+    plt.ylabel('objętość [m3]')
+    plt.plot(t_samples, v_samples, label='Objętość całkowita mieszanki w zbiorniku')
+    plt.title('V(t)')
+    plt.legend()
+
+    plt.figure('Prędkość przepływu strumienia sterowanego')
+    plt.xlabel('czas [s]')
+    plt.ylabel('prędkość przepływu [m3/s]')
+    plt.plot(t_samples, q1_samples)
+    plt.title('q(t)')
+
+    plt.figure('Stężenie w zbiorniku')
+    plt.xlabel('czas [s]')
+    plt.ylabel('stężenie [%]')
+    plt.plot(t_samples, ca_samples)
+    plt.title('ca(t)')
+
     plt.show()
 
 
-def main():
-    model()
+def save_to_file(title, data):
+    file = open(title + '.txt', 'w+')
+    for i in data:
+        file.write(str(i) + ';')
+    file.close()
 
 
 if __name__ == "__main__":
